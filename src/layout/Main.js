f5a4d2a3d84b4be7be54542e9a6222491ef15012
Ashikmahbub56 <ashikmahbub56@gmail.com>
2024-08-21 17:47:42 +0600

    router added

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { useLocation } from 'react-router-dom';
 
 const Main = () => {
+    const location = useLocation();
+    const noHeaderFooter = location.pathname.includes('login') ||
+    location.pathname.includes('signup');
     return (
         <div>
-            <Header></Header>
-            <main></main>
-            <Footer></Footer>
+            <div>
+        { noHeaderFooter || <Navbar /> }  // Navbar is shown unless noHeaderFooter is true
+        <Outlet />                        // The content of the child routes is rendered here
+        { noHeaderFooter || <Footer /> }  // Footer is shown unless noHeaderFooter is true
+    </div>
             
         </div>
     );
